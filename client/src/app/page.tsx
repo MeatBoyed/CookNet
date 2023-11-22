f35eb05629763dc77e0e8691f199cd6ef3510a9b
MeatBoyed <charlierossouw@outlook.com>
2023-11-22 07:53:58 +0200

    feat: Home page renderes recieps in db

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "~/server/db";
 import CallToAction from "~/utils/components/CallToAction";
 import CategoriesSection from "~/utils/components/CategoriesSelection";
 import Header from "~/utils/components/Header";
@@ -8,18 +9,27 @@ export const metadata = {
   description: "Just Do it - CookNet",
 };
 
-export default function Home() {
+export default async function Home() {
+  const recipes = await db.recipe.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
   return (
     <div className="container flex w-full flex-col items-center justify-center gap-12 px-4 py-8 ">
       <Header />
       <CategoriesSection />
       <div className="grid w-full grid-cols-2 gap-x-4 gap-y-10 ">
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
+        {recipes.map((recipe, index) => (
+          <RecipeCard recipe={recipe} key={index} />
+        ))}
       </div>
       {/* <CallToAction /> */}
     </div>
--- client/src/utils/components/RecipeCard.tsx
+++ client/src/utils/components/RecipeCard.tsx
@@ -1,30 +1,46 @@
 import Image from "next/image";
 import ImageThumbnail from "../../img/ImageThumbnail.png";
+import { Recipe } from "@prisma/client";
+import Link from "next/link";
 
-export default function RecipeCard() {
+interface RecipeProp {
+  title: string;
+  id: string;
+  description: string;
+  author: {
+    name: string | null;
+  };
+}
+
+export default function RecipeCard({ recipe }: { recipe: RecipeProp }) {
   return (
     <div className="flex w-full flex-col items-start justify-start gap-3">
-      <Image
-        height={300}
-        width={400}
-        src={ImageThumbnail}
-        alt="Recipe Thumbnail"
-        className="w-full"
-      />
+      <Link href={`/recipe/${recipe.id}`} className="w-full">
+        <Image
+          height={300}
+          width={400}
+          src={ImageThumbnail}
+          alt="Recipe Thumbnail"
+          className="w-full"
+        />
+      </Link>
       <div className="flex flex-col items-start justify-start gap-1">
         <p className="space-x-2 font-['Roboto'] text-xs font-semibold text-black">
           Meal
         </p>
-        <p className="font-['Roboto'] text-xl font-bold text-black">
-          Chocolate Cake
-        </p>
+        <Link
+          href={`/recipe/${recipe.id}`}
+          className="font-['Roboto'] text-xl font-bold text-black"
+        >
+          {recipe.title}
+        </Link>
         <p className="font-['Roboto'] text-sm font-normal text-black">
-          The best Chocolate cake in the world omd
+          {recipe.description}
         </p>
       </div>
       <div className="flex w-full items-center justify-start gap-2">
         <p className="font-['Roboto'] text-sm font-semibold leading-[21px] text-black">
-          Charles Rossouw
+          {recipe.author.name}
         </p>
         {/* <div className="font-['Roboto'] text-lg font-normal text-black">•</div> */}
         {/* <div className="font-['Roboto'] text-sm font-normal leading-[21px] text-black">
