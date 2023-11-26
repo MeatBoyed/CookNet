12b6a28210f9e5baea7dba3e4891f41c4104022b
MeatBoyed <charlierossouw@outlook.com>
2023-11-27 01:54:22 +0200

    fix: Tidied ActivityButtonsContainer

--- client/src/utils/components/RecipePage/ActivityButtonsContainer.tsx
+++ client/src/utils/components/RecipePage/ActivityButtonsContainer.tsx
@@ -1,6 +1,4 @@
-"use client";
 import { type Like, type Save } from "@prisma/client";
-import { useRouter } from "next/navigation";
 import { SaveRecipeBtn } from "./SaveRecipeBtn";
 import { LikeRecipeBtn } from "./LikeRecipeBtn";
 
@@ -14,16 +12,6 @@ export default function ActivityButtonsContainer({
   likedBy,
   savedBy,
 }: props) {
-  //   LikedBy.map((like) => {
-  //     if (like.userId == data?.user.id)
-  //   })
-
-  // TODO: Check if User has already Saved the Recipe (show correct UI)
-  // TODO: Allow user to Add to Saved Recipes & show correct UI if successful
-
-  // TODO: Check if User has already liked the Recipe (show correct UI)
-  // TODO: Allow user to like the Recipe & show correct UI if successful
-
   return (
     <div className="flex w-full flex-col items-start justify-center gap-3">
       <div className="flex items-center justify-center gap-10">
