b57bccf27146b6049c763a6dffc029c62074d482
MeatBoyed <charlierossouw@outlook.com>
2023-12-17 11:37:30 +0200

    feat: RecipeCard created

--- cooknet-v2/app/page.tsx
+++ cooknet-v2/app/page.tsx
@@ -1,10 +1,12 @@
+import RecipeCard from "@/components/RecipeCard";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello See Me</Button>
+      {/* <Button>Hello See Me</Button> */}
+      <RecipeCard />
     </main>
   );
 }
---
+++ cooknet-v2/components/RecipeCard.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import CheeseBurger from "../public/Alien Cheesburger.png";
+import { Badge } from "./ui/badge";
+import Link from "next/link";
+import { Dot } from "lucide-react";
+
+export default function RecipeCard() {
+  return (
+    <Card>
+      <Image src={CheeseBurger} alt="Thumbnail" className="w-full h-64" />
+      <CardHeader className="p-3">
+        <div className="flex justify-start items-center">
+          <Link href="/" className="text-xs">
+            Charles Rossouw
+          </Link>
+          <Dot />
+          <p className="text-xs">21/3/23</p>
+        </div>
+        <Link href="/charles/cheese">
+          <CardTitle>Alien Cheeseburger</CardTitle>
+        </Link>
+        <CardDescription>
+          An alien cheese burger is a delicious and exotic dish that can be made
+          with ingredients from other planets
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-start items-start gap-2 p-3 flex-wrap">
+        <Badge variant={"default"}>Cheese</Badge>
+        <Badge variant={"outline"}>Pickles</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+        <Badge variant={"secondary"}>Beef Patty</Badge>
+      </CardContent>
+    </Card>
+  );
+}
