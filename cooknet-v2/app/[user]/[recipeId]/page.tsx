94ada7036f842b152f1dfa3748459c128ae3c5ce
MeatBoyed <charlierossouw@outlook.com>
2023-12-17 12:05:28 +0200

    feat: Home page, User & Recipe routes defined, & Likes added to RecipeCard

--- cooknet-v2/app/[user]/[recipeId]/page.tsx
+++ cooknet-v2/app/[user]/[recipeId]/page.tsx
@@ -1,16 +1,16 @@
 import RecipeCard from "@/components/RecipeCard";
 
-export default function Home() {
+export default function Recipe() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 gap-10">
       {/* <Button>Hello See Me</Button> */}
       <h1 className="text-4xl font-extrabold tracking-widest text-center">
-        Today's Top Picks
+        Alien Cheese Burger
       </h1>
 
+      <p>Details</p>
+
       <div className="flex justify-start items-center flex-wrap gap-5">
-        <RecipeCard />
-        <RecipeCard />
         <RecipeCard />
       </div>
     </main>
--- cooknet-v2/app/[user]/page.tsx
+++ cooknet-v2/app/[user]/page.tsx
@@ -1,12 +1,14 @@
 import RecipeCard from "@/components/RecipeCard";
 
-export default function Home() {
+export default function User() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 gap-10">
       {/* <Button>Hello See Me</Button> */}
       <h1 className="text-4xl font-extrabold tracking-widest text-center">
-        Today's Top Picks
+        Charles Rossouw's Profile
       </h1>
+
+      <p>Your Recipes</p>
 
       <div className="flex justify-start items-center flex-wrap gap-5">
         <RecipeCard />
